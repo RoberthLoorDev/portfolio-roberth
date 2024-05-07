602cf7ada37edfcdea64561dee061fbd2c464463
RoberthLoorDev <dayvidlife@gmail.com>
2024-05-07 12:18:11 -0500

    add about me and thanks for watching section

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -14,6 +14,7 @@ import vue from './vue.png'
 import work from './work.png'
 import project1 from './project1.png'
 import project1capture from './project1capture.png'
+import me from './me.png'
 
 export {
   arrowHeader,
@@ -24,6 +25,7 @@ export {
   work,
   project1,
   project1capture,
+  me,
 }
 
 export const skillsArray = [
