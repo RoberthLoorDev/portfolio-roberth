1af8924b4bad8f4672f1bad561d7300422ccf01b
RoberthLoorDev <dayvidlife@gmail.com>
2024-05-12 23:06:19 -0500

    responsive design only for mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        xs: '320px', // Agregando el breakpoint xs de 320px
+      },
       colors: {
         background: '#090C1A',
         primary: '#2D8CFC',
