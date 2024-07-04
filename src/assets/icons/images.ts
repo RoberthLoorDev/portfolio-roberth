854f37c7a60fe45bd3717fb7914d68e9c6f03ab1
RoberthLoorDev <dayvidlife@gmail.com>
2024-07-04 17:13:13 -0500

    new skill icons and redesign of skills

---
+++ src/assets/icons/images.ts
@@ -0,0 +1,50 @@
+import arrowHeader from './arrow-button-header.png'
+import linkedin from './linkedin.png'
+import github from './github.png'
+import email from './email.png'
+import headerBackground from './header.jpg'
+import css from './css.png'
+import astro from './astro.png'
+import express from './express.png'
+import tailwind from './tailwind.png'
+import javascript from './javascript.png'
+import react from './react.png'
+import typescript from './typescript.png'
+import vue from './vue.png'
+import work from './work.png'
+import project1 from './project1.png'
+import project1capture from './project1capture.png'
+import me from './me.png'
+import education from './education.png'
+import iconProject1 from './project1.png'
+import iconProject2 from './projects/icon_proyect_2.png'
+import captureProject2 from './projects/capture_proyect_2.png'
+import instagram from './instagram.png'
+
+export {
+     arrowHeader,
+     email,
+     github,
+     linkedin,
+     headerBackground,
+     work,
+     project1,
+     project1capture,
+     me,
+     education,
+     iconProject1,
+     iconProject2,
+     captureProject2,
+     instagram,
+}
+
+export const skillsArray = [
+     react,
+     vue,
+     astro,
+     tailwind,
+     express,
+     typescript,
+     javascript,
+     css,
+]
---
+++ src/assets/skills/skillsImages.ts
@@ -0,0 +1,27 @@
+import react from './react.png'
+import astro from './astro.png'
+import css from './css.png'
+import html from './html.png'
+import javascript from './javascript.png'
+import mongodb from './mongodb.png'
+import nextjs from './nextjs.png'
+import nodejs from './nodejs.png'
+import postgresql from './postgresql.png'
+import tailwind from './tailwind.png'
+import typescript from './typescript.png'
+import vue from './vue.png'
+
+export const arraySkills = [
+     { icon: react, name: 'react' },
+     { icon: nextjs, name: 'nextjs' },
+     { icon: vue, name: 'vue' },
+     { icon: nodejs, name: 'nodejs' },
+     { icon: astro, name: 'astro' },
+     { icon: tailwind, name: 'tailwind' },
+     { icon: javascript, name: 'javascript' },
+     { icon: typescript, name: 'typescript' },
+     { icon: postgresql, name: 'postgresql' },
+     { icon: mongodb, name: 'mongodb' },
+     { icon: html, name: 'html' },
+     { icon: css, name: 'css' },
+]
