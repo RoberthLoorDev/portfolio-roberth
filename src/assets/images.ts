eebe57ec693fac8fe6d3090207e46d6d5b143747
RoberthLoorDev <dayvidlife@gmail.com>
2024-07-04 14:12:45 -0500

    header changes suggested by the midudev community

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -19,30 +19,32 @@ import education from './education.png'
 import iconProject1 from './project1.png'
 import iconProject2 from './projects/icon_proyect_2.png'
 import captureProject2 from './projects/capture_proyect_2.png'
+import instagram from './instagram.png'
 
 export {
-  arrowHeader,
-  email,
-  github,
-  linkedin,
-  headerBackground,
-  work,
-  project1,
-  project1capture,
-  me,
-  education,
-  iconProject1,
-  iconProject2,
-  captureProject2,
+     arrowHeader,
+     email,
+     github,
+     linkedin,
+     headerBackground,
+     work,
+     project1,
+     project1capture,
+     me,
+     education,
+     iconProject1,
+     iconProject2,
+     captureProject2,
+     instagram,
 }
 
 export const skillsArray = [
-  react,
-  vue,
-  astro,
-  tailwind,
-  express,
-  typescript,
-  javascript,
-  css,
+     react,
+     vue,
+     astro,
+     tailwind,
+     express,
+     typescript,
+     javascript,
+     css,
 ]
