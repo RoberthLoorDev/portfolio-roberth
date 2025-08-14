00f214e65334a78168e200f11ce21424221dfbd9
RoberthLoorDev <dayvidlife@gmail.com>
2025-08-13 20:52:39 -0500

    add: incluir nuevas imágenes de habilidades (MySQL, NestJS, Shadcn) y ajustar estilo en la sección de habilidades

--- src/assets/skills/skillsImages.ts
+++ src/assets/skills/skillsImages.ts
@@ -10,6 +10,9 @@ import postgresql from './postgresql.png'
 import tailwind from './tailwind.png'
 import typescript from './typescript.png'
 import vue from './vue.png'
+import shadcn from './shadcn.png'
+import mysql from './mysql.png'
+import nestjs from './nestjs.png'
 
 export const arraySkills = [
      { icon: react, name: 'react' },
@@ -24,4 +27,7 @@ export const arraySkills = [
      { icon: mongodb, name: 'mongodb' },
      { icon: html, name: 'html' },
      { icon: css, name: 'css' },
+     { icon: shadcn, name: 'shadcn' },
+     { icon: mysql, name: 'mysql' },
+     { icon: nestjs, name: 'nestjs' },
 ]
